54422b2fdaffa1c5d5ed70f6f32c9bd3ef8fa663
deep-git <deepakthangella5@gmail.com>
2024-03-08 20:11:23 -0500

    Add initial code and files

---
+++ components/LoadMore.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import { fetchAnime } from "@/app/action";
+import AnimeCard from "./AnimeCard";
+
+let page = 2;
+
+export type AnimeCard = JSX.Element;
+
+function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeCard[]>([]);
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        setData([...data, ...res]);
+        page++;
+      });
+    }
+  }, [inView, data]);
+
+  return (
+    <>
+      {data}
+      <section className="flex justify-center items-center w-full">
+        <div ref={ref}>
+          <Image
+            src="./spinner.svg"
+            alt="spinner"
+            width={56}
+            height={56}
+            className="object-contain"
+          />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default LoadMore;
